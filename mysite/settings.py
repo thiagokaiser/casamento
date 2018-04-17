d37c7401a5a8a8ec27f51013095692f91feef493
Thiago Kaiser <thiago.kaiser@hotmail.com>
2018-04-17 11:28:53 -0300

    allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = '88&1q#vge^5k6d$_0@o79)5dyee(1k8)mbj6%29nb5=v*yao-4'
 DEBUG = True
 
 ALLOWED_HOSTS = ['kaiserz.pythonanywhere.com',
+                 'olgafrancis.pythonanywhere.com',
                  '127.0.0.1'
                 ]
 
